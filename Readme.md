911eee69f61cb405f97319743e1a360b8ca7d69a
Josef Leyva <jalm_x@hotmail.com>
2025-08-12 10:57:53 -0600

    fix

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Extract list from Jitsi Meet
+
+## How to use
+
+For to use the script, fist you have to click over participants (like show). Then, you have to save the **page web complete**
+
+![example](assets/example.png)
+
+Then exec the script
+
+```bash
+list_jitsi.py name_file.html
+```
+
+## Details
+
+- Have to change permissions from script
+  - `sudo chmod +x list_jitsi.py`
+- Or you have to add to `$PATH` system
---
+++ src/list_jitsi.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import os
+from datetime import datetime
+import csv
+from sys import argv
+from bs4 import BeautifulSoup
+import re
+
+DEBUG = True
+
+HELP = """
+How to use:
+
+    list_jitis file.html
+
+"""
+
+
+def read_argv():
+
+    if len(argv) == 2:
+        return argv[1]
+
+    print("No indicated file to extact list")
+    print(HELP)
+    exit(1)
+
+
+def read_file(path: str):
+    if os.path.exists(path):
+        with open(path) as file:
+            return file.read()
+    print(f"Not exist file: {path}")
+    print(HELP)
+    exit(1)
+
+
+def extact_list(txt):
+    html = BeautifulSoup(txt, "html.parser")
+    list_people = html.find_all(attrs={"class": "display-name"})
+    people = set()
+    for person in list_people:
+
+        name = person.get_text(strip=True).replace("(me)", "").replace("\n", " ")
+        name = re.sub(" +", " ", name)
+        people.add(name)
+
+    return people
+
+
+def save_csv(list_to_save):
+
+    if not len(list_to_save):
+        print("Nothing to save! :(")
+        print(HELP)
+        exit(1)
+
+    name_csv = f"lista_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv"
+
+    with open(name_csv, mode="+w", encoding="utf-8") as list_csv:
+        file_csv = csv.writer(list_csv)
+        file_csv.writerow(["Nombre"])
+        count = 0
+        for p in list_to_save:
+            file_csv.writerow([p])
+            count += 1
+
+    print(f"{count} names")
+    print(f"File savef as: {name_csv}")
+
+
+def main():
+    file_name = read_argv()
+    txt = read_file(file_name)
+    people = extact_list(txt)
+    save_csv(people)
+
+
+if __name__ == "__main__":
+    main()
